b9731a9fdcffced3a6d7a5c5fb537dd02d6d1e1a
joao <jlucasoc@hotmail.com>
2021-12-20 21:34:17 -0300

    Literal types 🔥

---
+++ src/literal-types/literal-types.ts
@@ -0,0 +1,21 @@
+let n = 10;
+n = 0b1010;
+console.log(n, "n is a number type");
+
+const m = 10;
+console.log(m, 'm is a "10" type');
+
+let b = 100 as const; // eslint-disable-line
+
+const person = {
+    name: "Abacaxiguy" as const,
+    age: 18,
+};
+
+console.log(person);
+
+function chooseColor(color: "Red" | "Green" | "Yellow") {
+    return color;
+}
+
+console.log(chooseColor("Red"));
