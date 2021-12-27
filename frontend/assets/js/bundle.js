5a51dbc6823ccd45b4078ad32836f351184c05ee
joao <jlucasoc@hotmail.com>
2021-12-26 22:43:18 -0300

    Configuring webpack 🧊

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/webpack/index.ts ***!
+  \******************************/
+
+console.log("Hello World!");
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
