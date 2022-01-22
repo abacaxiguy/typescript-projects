256bef46ce0abfa80e7fd52a5df9c68ab9435f03
joao <jlucasoc@hotmail.com>
2022-01-21 22:06:48 -0300

    ES2020 Operators ✨🆕

---
+++ src/es2020-operators/es2020-operators.ts
@@ -0,0 +1,17 @@
+type TheDocument = {
+    title: string;
+    text: string;
+    date?: Date;
+};
+
+const theDocument: TheDocument = {
+    title: "The title",
+    text: "The text",
+};
+
+console.log(theDocument.date?.toDateString() ?? "it doesnt have 'date'");
+console.log(undefined ?? "it doesnt have 'date' x2");
+console.log(null ?? "it doesnt have 'date' x3");
+console.log(false ?? "it doesnt have 'date' x4");
+console.log(0 ?? "it doesnt have 'date' x5");
+console.log("" ?? "it doesnt have 'date' x5");
