f16ce3642003356339ab4cc8e060da792127995e
joao <jlucasoc@hotmail.com>
2022-02-12 22:42:56 -0300

    Generics 🧬

--- src/es2020-operators/es2020-operators.ts
+++ src/es2020-operators/es2020-operators.ts
@@ -14,4 +14,4 @@ console.log(undefined ?? "it doesnt have 'date' x2");
 console.log(null ?? "it doesnt have 'date' x3");
 console.log(false ?? "it doesnt have 'date' x4");
 console.log(0 ?? "it doesnt have 'date' x5");
-console.log("" ?? "it doesnt have 'date' x5");
+console.log("" ?? "it doesnt have 'date' x6");
