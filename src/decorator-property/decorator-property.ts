c799fb28f5ce3cfb93148c623c1155e9053a8b5b
joao <jlucasoc@hotmail.com>
2022-03-18 22:17:10 -0300

    Decorator property ✨🥛

---
+++ src/decorator-property/decorator-property.ts
@@ -0,0 +1,49 @@
+function decorator(classPrototype: any, methodName: string | symbol): any {
+    let propertyValue: any;
+
+    return {
+        get: () => propertyValue,
+        set: (value: any) => {
+            if (typeof value === "string") {
+                propertyValue = value.split("").reverse().join("");
+                return;
+            }
+            propertyValue = value;
+        },
+    };
+}
+
+export class OnePerson {
+    @decorator
+    name: string;
+    @decorator
+    lastName: string;
+    @decorator
+    age: number;
+
+    constructor(name: string, lastName: string, age: number) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    method(msg: string): string {
+        return `${this.name} ${this.lastName}: ${msg}`;
+    }
+
+    get fullName(): string {
+        return this.name + " " + this.lastName;
+    }
+
+    set fullName(value: string) {
+        const words = value.split(/\s+/g);
+        const firstName = words.shift();
+        if (!firstName) return;
+        this.name = firstName;
+        this.lastName = words.join(" ");
+    }
+}
+
+const person = new OnePerson("me", "meeseek", 9);
+const method = person.method("its me");
+console.log(method);
